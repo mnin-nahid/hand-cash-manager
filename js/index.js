0d7eb55858434148c9092a620f022aa7a5df9fc4
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-02-17 12:07:55 +0600

    calculation complete

--- js/index.js
+++ js/index.js
@@ -11,4 +11,15 @@ document.getElementById('claculate-button').addEventListener('click', function()
     totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
     document.getElementById('total-expenses').innerText = totalExpenses;
     document.getElementById('Balance-after-expenses').innerText = incomeAmount - totalExpenses;
+});
+document.getElementById('save-button').addEventListener('click', function(){
+    // console.log('save is working')
+    // income
+    const incomeAmount = parseFloat(document.getElementById('income-amount').value);
+    const percentOfSaving = parseFloat(document.getElementById('percent-of-saving').value);
+    const savingAmount = (incomeAmount / 100) * percentOfSaving;
+    document.getElementById('saving-amount').innerText = savingAmount;
+    const totalExpenses = parseFloat(document.getElementById('total-expenses').innerText);
+    document.getElementById('remaining-balance').innerText = incomeAmount - savingAmount - totalExpenses;
+
 })
